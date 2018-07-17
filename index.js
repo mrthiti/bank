77dd7dae6e275fdc3db3b318eda6718d322279d5
Thiti Yamsung <t.yamsung@gmail.com>
2018-07-17 13:10:39 +0700

    Refactor code

--- index.js
+++ index.js
@@ -7,16 +7,28 @@ const createAccount = () => {
   console.log("--- Create Account ---")
   let accountName = readline.question("Enter account name: ")
 
+  if (account.some((item) => accountName == item.name)) {
+    console.log('Account name is already.')
+    return
+  }
+
   account.push({
     name: accountName,
     balance: 0
   })
+
+  console.log('Account create.')
 }
 
 const depositMoney = () => {
-  console.log("--- Deposit Account ---")
+  console.log("--- Deposit Money ---")
   let accountName = readline.question("Enter account name: ")
   let money = parseInt(readline.question("Enter money: "))
+
+  if (!account.some((item) => accountName == item.name)) {
+    console.log('No Account!!!')
+    return
+  }
 
   account.map(item => {
     if (item.name == accountName) return item.balance = money
@@ -25,11 +37,47 @@ const depositMoney = () => {
   })
 }
 
+const withdrawMoney = () => {
+  console.log("--- Withdraw Money ---")
+  let accountName = readline.question("Enter account name: ")
+  let money = parseInt(readline.question("Enter money: "))
+
+  if (!account.some((item) => accountName == item.name)) {
+    console.log('No Account!!!')
+    return
+  }
+
+  account.map(item => {
+    if (item.name == accountName) return item.balance -= money
+
+    return item
+  })
+}
+
 const checkBalance = () => {
   console.log("--- Check balance ---")
   let accountName = readline.question("Enter your account name: ")
+
+  if (!account.some((item) => accountName == item.name)) {
+    console.log('No Account!!!')
+    return
+  }
+
   let balance = account.find(item=>item.name == accountName)
-  console.log(balance)
+  console.log(`Amount ${balance.balance}`)
+}
+
+const displayAccountDetail = () => {
+  console.log("--- Display account deatil ---")
+  let accountName = readline.question("Enter your account name: ")
+  let accountDatail = account.find(item => item.name == accountName)
+
+  if (!account.some((item) => accountName == item.name)) {
+    console.log('No Account!!!')
+    return
+  }
+
+  console.log(accountDatail)
 }
 
 const increaseYear = () => {
@@ -49,7 +97,9 @@ while (true) {
 
   console.log("1. Create Account")
   console.log("2. Deposit money")
+  console.log("3. Withdraw money")
   console.log("4. Check balance")
+  console.log("5. Display account detail")
   console.log("6. Increase year")
   let option = readline.question("Enter option number: ")
 
@@ -60,11 +110,19 @@ while (true) {
     case "2":
       depositMoney()
       break
+    case "3":
+      withdrawMoney()
+      break
     case "4":
       checkBalance()
+      break
+    case "5":
+      displayAccountDetail()
       break
     case "6":
       increaseYear()
       break
+    default:
+      console.log("No option!!!")
   }
 }
